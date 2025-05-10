cb96b4e982b858613e4637487b704515a944e78c
Danielle <daniellebraganca9@gmail.com>
2025-05-10 17:19:03 -0300

    Add files via upload

---
+++ Guardião do Cofre/src/GuardiaoDoCofre.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GuardiaoDoCofre {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o código númerico: ");
+        int codigo = sc.nextInt();
+
+        System.out.print("Digite o nível de segurança (1 a 5): ");
+        int nivel = sc.nextInt();
+
+        System.out.print("Digite a chave especial (A, B ou C): ");
+        char chave = sc.next().toLowerCase().charAt(0);
+
+        if (nivel < 1 || nivel > 5 || (chave != 'A' && chave != 'B' && chave != 'C')) {
+            System.out.println("Entrada inválida!");
+        } else if ((codigo % 7 == 0 && nivel >= 3) || chave == 'A') {
+            System.out.println("Cofre aberto!");    
+        } else {
+            System.out.println("Acesso negado!");
+        }
+    
+    }
+}
